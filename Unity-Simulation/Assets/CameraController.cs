3993028922a762a97b624b1f4800f8d0d261bbb4
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2023-10-31 18:00:43 -0600

    added capsile and camera controller
    added capsule and camera controller to the project as a little test simulation of what it will look like

---
+++ Unity-Simulation/Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 30.0f;
+    public float translationSpeed = 3.0f;
+
+    void Update()
+    {
+        // Rotation with A and D keys
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+
+        // Translation with W and S keys
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * translationSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * translationSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
+
