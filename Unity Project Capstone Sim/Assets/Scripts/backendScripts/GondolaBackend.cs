30d5223d57d94142d5d0e00b78b607afcf335e9b
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-03-04 20:48:20 -0700

    added gondola animation and model
    added large file but set up git large file pushing on my computer

--- Unity Project Capstone Sim/Assets/Scripts/backendScripts/GondolaBackend.cs
+++ Unity Project Capstone Sim/Assets/Scripts/backendScripts/GondolaBackend.cs
@@ -6,7 +6,23 @@ Script to handle the button inputs and calculations for the gondola
 using UnityEngine;
 
 public class GondolaBackend : MonoBehaviour {
-    public void Reset(){
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+
+    public void Reset() {
         Debug.Log("Gondola is reset ");
+    }
+
+
+    public void PlayGondola() {
+        animator.SetTrigger("PlayAllGondolas");
+    }
+    public void PlayReverseGondola() {
+        animator.SetTrigger("PlayReverseGondolas");
     }
 }
