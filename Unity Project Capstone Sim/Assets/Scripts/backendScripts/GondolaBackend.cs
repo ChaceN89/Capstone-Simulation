3eeec8f9111b42714ee93b84ada5b5d16147a8ab
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-03-15 18:13:25 -0600

    set up multiple cameras

--- Unity Project Capstone Sim/Assets/Scripts/backendScripts/GondolaBackend.cs
+++ Unity Project Capstone Sim/Assets/Scripts/backendScripts/GondolaBackend.cs
@@ -4,11 +4,14 @@ GondolaBackend.cs
 Script to handle the button inputs and calculations for the gondola
 */
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GondolaBackend : MonoBehaviour {
 
+    public Canvas camCanvas;
     private Animator animator;
 
+    public Toggle camToggle; // 
     public ToggleList[] toggleList; // list of toggles and items for tooggle to hide or show (visibility )
     private GondolaCamera gondolaCamera; // The active camera for this scenerio 
 
@@ -21,11 +24,18 @@ public class GondolaBackend : MonoBehaviour {
     }
     public void Update() {
         backendFunctions.CheckVisibility(toggleList);
+
+        camCanvas.enabled=camToggle.isOn;
+        
     }
 
     public void Reset() {
+        Debug.Log("Reset Gondola");
         gondolaCamera.ResetSnap();
+        camToggle.isOn = false;
+        
     }
+
 
     public void SetSnap(string newSnap) {
         gondolaCamera.SetSnap(newSnap);
