dbdf28701d18b4072c47233d0b661770fba8c78a
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-03-16 21:17:29 -0600

    added skybox for underwater and ordered some functions which lead to reseting outline materials

---
+++ Unity Project Capstone Sim/Assets/Scripts/CamUnderwaterEffect.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CamUnderwaterEffect : MonoBehaviour {
+    // Materials for above water and below water 
+    public Material aboveWaterSkybox;
+    public Material underwaterSkybox;
+
+    // Minimal fog density for underwater
+    public float minimalFogDensity = 0.0015f; // Adjust this value to your preference for fog distance
+
+    void Update() {
+        float yPosition = transform.position.y;
+
+        if (yPosition >= 0) {
+            // Set skybox to above water
+            RenderSettings.skybox = aboveWaterSkybox;
+
+            // Turn off fog
+            RenderSettings.fog = false;
+        }
+        else {
+            // Underwater
+
+            // Turn on fog with minimal density
+            RenderSettings.fog = true;
+            RenderSettings.fogDensity = minimalFogDensity; // Use a constant value for simplicity
+
+            // Change the skybox to underwater
+            RenderSettings.skybox = underwaterSkybox;
+        }
+    }
+}
